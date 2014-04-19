9f4e7ebea1def56130725d062400cdfccd8d3c46
gus-home <gus@gusperez.com>
2014-04-19 08:30:49 -0700

    - Eula presenter now uses custom buttons on the message box: accept and reject - Took a dependency on the Silverlight Toolkit to use the CustomMessageBox - Refactored the EulaPresenter code a fair amount

--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using TestApp.Resources;
 using ZinkoLabs.AppFx.StartupNotifications;
+using ZinkoLabs.AppFx.EulaManagement;
 
 namespace TestApp
 {
@@ -27,31 +28,53 @@ namespace TestApp
         private void InitializeStartupNotifications()
         {
             var disclaimerText = "Your disclaimer text goes here.";
-            _eulaPresenter = new EulaPresenter("Test App", 103, disclaimerText);
+            _eulaPresenter = new EulaPresenter("Test App", 104, disclaimerText);
             _notificationsManager = new StartupNotificationsManager("Test App",
                 new StartupNotification(202, "What's new?", "We've added:\n• New feature\n• Another feature\n• Minor bug fixes"));
 
-            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!_eulaPresenter.PresentEulaToUser())
+            EnsureEulaIsAccepted();
+        }
+
+        /// <summary>
+        /// Handles showing the EULA to the user and transitioning states once
+        /// it has gotten the user's response to the terms.
+        /// </summary>
+        private void EnsureEulaIsAccepted()
+        {
+            if (_eulaPresenter.EulaResponseNeeded)
             {
-                DisableAllControls();
-            }
-            else
-            {
-                _notificationsManager.ShowNotifications();
+                _eulaPresenter.Answered += (s1, e1) => {
+                    if (e1.EulaAccepted)
+                    {
+                        _notificationsManager.ShowNotifications();
+                    }
+                    else
+                    {
+                        RestrictFunctionalityAfterEulaRejection();
+                    }
+                };
+
+                _eulaPresenter.PresentEulaToUser();
             }
         }
 
-        private void DisableAllControls()
+        /// <summary>
+        /// Appropriately enables and disables functionality in response
+        /// to the user rejecting the app's terms.
+        /// </summary>
+        private void RestrictFunctionalityAfterEulaRejection()
         {
+            // Show your EULA rejection information
             EulaRejectedText.Visibility = System.Windows.Visibility.Visible;
+
+            // Hide your app's actual functionality
             TitlePanel.Visibility = System.Windows.Visibility.Collapsed;
             ContentPanel.Visibility = System.Windows.Visibility.Collapsed;
-            ApplicationBar.IsVisible = false;
         }
     }
 }
---
+++ ZinkoLabs.AppFx/EulaManagement/EulaPresenter.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+using System.Windows;
+
+namespace ZinkoLabs.AppFx.EulaManagement
+{
+    public class EulaPresenter
+    {
+        IsolatedStorageSettings _settings = IsolatedStorageSettings.ApplicationSettings;
+        bool _eulaResponseNeeded;
+        bool _userAcceptedEula;
+        string _appName;
+        int _appVersion;
+        string _eulaContent;
+        CustomMessageBox _eulaMessageBox;
+        const string _eulaAcceptanceResult = "ZinkoLabs.AppFx.EulaPresentation.UserAcceptedEula";
+
+        public EulaPresenter(string appName, int appVersion, string eulaContent)
+        {
+            _appName = appName;
+            _appVersion = appVersion;
+            _eulaContent = eulaContent;
+            _eulaResponseNeeded = !HasEulaAlreadyBeenAccepted();
+        }
+
+        public bool EulaResponseNeeded
+        {
+            get { return _eulaResponseNeeded; }
+        }
+
+        public bool UserAcceptedEula
+        {
+            get { return _userAcceptedEula; }
+        }
+
+        public event EulaAnsweredEventHandler Answered;
+
+        public void PresentEulaToUser()
+        {
+            _eulaMessageBox = new CustomMessageBox() {
+                Caption = string.Format("{0} App Terms", _appName),
+                Message = _eulaContent,
+                LeftButtonContent = "accept",
+                RightButtonContent = "reject"
+            };
+
+            _eulaMessageBox.Dismissed += (s1, e1) => {
+                bool result = (e1.Result == CustomMessageBoxResult.LeftButton);                
+                if (result)
+                {
+                    _userAcceptedEula = true;
+                    _eulaResponseNeeded = false;
+                    SaveAcceptedEulaState();
+                }
+
+                Answered(this, new EulaAnsweredEventArgs(result));
+            };
+
+            _eulaMessageBox.Show();
+        }
+
+        private void SaveAcceptedEulaState()
+        {
+            _settings[_eulaAcceptanceResult] = _appVersion;
+        }
+
+        private bool HasEulaAlreadyBeenAccepted()
+        {
+            if (_settings.Contains(_eulaAcceptanceResult))
+            {
+                return (_appVersion <= (int)_settings[_eulaAcceptanceResult]);
+            }
+
+            return false;
+        }
+    }
+
+    public delegate void EulaAnsweredEventHandler(object sender, EulaAnsweredEventArgs e);
+
+    public class EulaAnsweredEventArgs
+    {
+        bool _eulaAccepted;
+
+        public EulaAnsweredEventArgs(bool isAccepted)
+        {
+            _eulaAccepted = isAccepted;
+        }
+
+        public bool EulaAccepted
+        {
+            get { return _eulaAccepted; }
+        }
+    }
+}
